5e3062b5b0a893e6d279b7167c9e03fdfaaf1f2a
Andriy-sys <marchycandriy@gmail.com>
2022-02-09 21:28:35 +0200

    MOD

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 function Click() {
-  const inp = document.getElementById('email');
+  let inp = document.getElementById('email');
+  let rep = document.getElementById('psw-repeat');
   let inp2 = document.getElementById('psw');
   let emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
   let passwor =
@@ -7,14 +8,64 @@ function Click() {
   if (emailPattern.test(inp.value) != true) {
     let p = document.getElementById('out');
     p.innerHTML = `Wrong email address`;
-  } else if (passwor.test(inp2.value) != true) {
-    let p = document.getElementById('out2');
-    p.innerHTML = `Passwords must match`;
   } else {
-    document.location.href = 'http://ru.stackoverflow.com';
-    return false;
+    setCookie('email', inp.value);
+  }
+  if (passwor.test(inp2.value) != true) {
+    let p = document.getElementById('out2');
+    p.innerHTML = `Passwords wrong`;
+  }
+
+  if (inp2.value.includes(rep.value) != true) {
+    let p = document.getElementById('rep');
+    p.innerHTML = `Passwords must match`;
   }
 }
 
-let p = document.getElementById('hell');
-p.innerHTML = `Hello,${inp.value}`;
+function setCookie(cname, cvalue) {
+  let d = new Date();
+  d.setTime(new Date().getTime() + 60 * 60 * 1000);
+  let expires = 'expires=' + d.toGMTString();
+  document.cookie = cname + '=' + cvalue + '; ' + expires;
+}
+
+function getCookie(cname) {
+  var name = cname + '=';
+  var ca = document.cookie.split(';');
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i].trim();
+    if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
+  }
+  return '';
+}
+
+function Load() {
+  let p = document.getElementById('inf');
+  p.innerHTML = `Hello ${getCookie('email')}!`;
+  let a = document.createElement('a');
+  a.href = `index.html`;
+  a.text = ' exit';
+  p.appendChild(a);
+}
+
+function Check() {
+  let name = document.getElementById('fname').value;
+  let lastname = document.getElementById('lname').value;
+  let t1 = /^[A-Za-z]+$/;
+  if (
+    t1.test(name) &&
+    t1.test(lastname) &&
+    name.length > 1 &&
+    name.length < 20 &&
+    lastname.length > 1 &&
+    lastname.length < 20
+  ) {
+    setCookie('name', name);
+    setCookie('lastname', lastname);
+  } else {
+    let p = document.getElementById('nm');
+    let p1 = document.getElementById('ln');
+    p1.innerHTML = 'Registry error';
+    p.innerHTML = 'Registry error';
+  }
+}
